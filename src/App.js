94fede0fdcc46f7738799c5a431f985bb27d4807
Gyeongsu1997 <choiiisu11@gmail.com>
2022-10-07 15:43:08 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import Customer from './components/Customer'
+/* eslint-disable */
+import { useState } from 'react';
+import Customer from './components/Customer';
 import './App.css';
 
+/*
 const customers = [
 {
   'id': 1,
@@ -26,9 +29,36 @@ const customers = [
   'gender': '남자',
   'job': '디자이너'
 }
-]
+]*/
 
 function App() {
+  let [customers, 변경] = useState([
+    {
+      'id': 1,
+      'image': 'https://placeimg.com/64/64/1',
+      'name': '최경수',
+      'birthday': '971111',
+      'gender': '남자',
+      'job': '대학생'
+    },
+    {
+      'id': 2,
+      'image': 'https://placeimg.com/64/64/2',
+      'name': '김경수',
+      'birthday': '970211',
+      'gender': '여자',
+      'job': '프로그래머'
+    },
+    {
+      'id': 3,
+      'image': 'https://placeimg.com/64/64/3',
+      'name': '정범수',
+      'birthday': '921131',
+      'gender': '남자',
+      'job': '디자이너'
+    }
+    ]);
+
   return (
     <div>
       {
